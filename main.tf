b61f54e23b1c8962f869979620c2fd75e27c3f89
ukoyande <uday.koyande@gmail.com>
2024-08-13 17:54:03 -0500

    fixinf errors

--- main.tf
+++ main.tf
@@ -34,6 +34,20 @@ module "blog_vpc" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name    = "blog_new"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -87,19 +101,7 @@ module "alb" {
 }
 
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name    = "blog_new"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
 
 resource "aws_security_group" "blog" {
   name        = "blog"
